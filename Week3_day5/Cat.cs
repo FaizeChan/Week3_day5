452cd480085f5073370feceb4fa801d3a395a566
FaizeChan <obey.faize@gmail.com>
2017-10-13 10:42:38 -0400

    initial commit

---
+++ Week3_day5/Cat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day5
+{
+    class Cat
+    {
+
+        private int catCount;
+
+
+        public int CatCount
+        {
+            get { return this.catCount; }
+            set { this.catCount = value; }
+        }
+
+        public Cat()
+        {
+
+        }
+
+        public void WarCry()
+        {
+            Console.WriteLine("We are a army of {0} cats!", catCount);
+        }
+
+    }
+}
---
+++ Week3_day5/Turtle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_day5
+{
+    class Turtle
+    {
+        //Tis static variable belongs to class
+        //Tis is shared among all instances.
+        //Tis also means we can use the variable without
+        //creating instances first.
+        static int turtleCount;
+
+        public Turtle()
+        {
+            turtleCount++;
+        }
+
+        //Tis static Method belongs to the class.
+        //It's shared among all instances.
+        //It means we can use the Method without creating
+        //any instances first.
+        public static void WarCry()
+        {
+            Console.WriteLine("We are an army of {0} turtles", turtleCount);
+        }
+    }
+}
